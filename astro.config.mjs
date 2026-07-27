9e2ede12a7f70c93703b5da54f5c36e587108fbc
hoiril.mochtar <horimo.me@gmail.com>
2026-07-27 12:25:31 +0700

    feat: implement portfolio base layout, navigation, and skill components using Tailwind CSS and Vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    site: 'https://hoirilm.github.io',
-    integrations: [tailwind()],
+  site: 'https://hoirilm.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
